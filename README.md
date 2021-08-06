8fdd4e4e0c4614bc4c28850dc4c8a3e86d1bccf5
DevHyperCoder <arduinoleo88@gmail.com>
2021-08-06 08:12:01 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# marky - the new way to manage bookmarks
+
+## LICENSE
+
+`marky` is licensed under the GNU General Public License Version 3.0 | Our copy of the license can be found [here](./LICENSE)
---
+++ app/src/main/java/com/devhypercoder/marky/MainActivity.kt
@@ -0,0 +1,29 @@
+/*
+marky - the new way to manage bookmarks
+Copyright (C) 2021  DevHyperCoder
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.devhypercoder.marky
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
